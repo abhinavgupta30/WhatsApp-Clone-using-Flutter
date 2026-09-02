fca09383f2e3d70ab1bdd7982b2d6caf08699728
Abhinav Gupta <abhiguptaa41@gmail.com>
2026-09-02 21:44:10 +0530

    Fix Firebase configuration and web deployment

--- WhatsAppClone-main/WhatsAppClone-main/whatsapp/lib/main.dart
+++ WhatsAppClone-main/WhatsAppClone-main/whatsapp/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp/LoginScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:whatsapp/LoginScreen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  
-  runApp(MyApp());
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
